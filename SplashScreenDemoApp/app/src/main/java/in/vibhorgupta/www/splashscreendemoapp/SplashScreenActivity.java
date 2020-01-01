fbd752e78efefe576a7cc50275e4d96c5cc18421
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2020-01-01 16:29:22 +0530

    Add files via upload

---
+++ SplashScreenDemoApp/app/src/main/java/in/vibhorgupta/www/splashscreendemoapp/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package in.vibhorgupta.www.splashscreendemoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        int SPLASH_TIME_OUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
